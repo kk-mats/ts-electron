1f5730392dcac5417d796329b6a97551d7bb3982
kk-mats <k.mats.kk@gmail.com>
2020-03-07 22:39:17 +0900

    chore: Create build configurations

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+	extends: [
+		"airbnb",
+		"prettier/@typescript-eslint",
+		"plugin:react/recommended",
+		"plugin:prettier/recommended",
+		"plugin:jsx-a11y/recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:import/typescript",
+		"prettier",
+		"prettier/react"
+	],
+	plugins: [
+		"@typescript-eslint",
+		"react",
+		"prettier",
+		"jsx-a11y",
+		"import"
+	],
+	rules:{
+		"react/jsx-filename-extension": [1, { "extensions": [".tsx"] }],
+		"prettier/prettier": [
+			"error",
+			{
+				tabWidth: 4,
+				useTabs: true
+			}
+		]
+	},
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true
+		},
+		project: "tsconfig.json"
+	},
+	globals: {
+		document: true
+	},
+	settings: {
+		"import/parsers": {
+			"@typescript-eslint/parser": [".ts", ".tsx"]
+		},
+		"import/resolver": {
+			typescript: {
+				directory: "./tsconfig.json"
+			}
+		},
+		propWrapperFunctions: [
+			// The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+			"forbidExtraProps",
+			{property: "freeze", object: "Object"},
+			{property: "myFavoriteWrapper"}
+		],
+		linkComponents: [
+			// Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+			"Hyperlink",
+			{name: "Link", linkAttribute: "to"}
+		]
+
+	}
+}
---
+++ webpack.config.dev.ts
@@ -0,0 +1,79 @@
+import * as path from "path";
+import * as webpack from "webpack";
+import * as HtmlWebpackPlugin from "html-webpack-plugin";
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+
+const common: webpack.Configuration = {
+	mode: "development"
+};
+
+const dist = path.resolve(__dirname, "dist");
+const resolve = {
+	plugins: [
+		new TsconfigPathsPlugin({
+			configFile: "tsconfig.json"
+		})
+	]
+};
+
+const main: webpack.Configuration = {
+	...common,
+	target: "electron-main",
+	devtool: "inline-source-map",
+	entry: path.resolve(__dirname, "src", "main", "index.ts"),
+	output: {
+		path: dist,
+		filename: "main.js"
+	},
+	resolve,
+	module: {
+		rules: [
+			{
+				enforce: "pre",
+				test: /\.ts$/,
+				loader: "eslint-loader",
+				include: path.resolve(__dirname, "src", "main")
+			},
+			{
+				test: /\.tsx?$/,
+				loader: "ts-loader",
+				include: path.resolve(__dirname, "src", "main")
+			}
+		]
+	}
+};
+
+const renderer: webpack.Configuration = {
+	...common,
+	target: "electron-renderer",
+	devtool: "inline-source-map",
+	entry: path.resolve(__dirname, "src", "renderer", "index.tsx"),
+	output: {
+		path: dist,
+		filename: "renderer.js"
+	},
+	resolve,
+	module: {
+		rules: [
+			{
+				enforce: "pre",
+				test: /\.tsx?$/,
+				loader: "eslint-loader",
+				include: path.resolve(__dirname, "src", "renderer")
+			},
+			{
+				test: /\.tsx?$/,
+				loader: "ts-loader",
+				include: path.resolve(__dirname, "src", "renderer")
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "src", "index.html"),
+			filename: path.resolve(dist, "index.html")
+		})
+	]
+};
+
+export default [main, renderer];
