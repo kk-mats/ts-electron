224f350cab7b1da1e7b7bec917cc10920f32f26c
kk-mats <k.mats.kk@gmail.com>
2020-03-11 17:13:29 +0900

    feat: Introduce react components to html

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,8 +1,20 @@
 import * as React from "react";
 import * as ReactDom from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import { ThemeProvider } from "@material-ui/core";
+import theme from "renderer/styles/theme";
+
+import Root from "renderer/components/pages/Root";
 
 const Index: React.FunctionComponent = () => {
-	return <div>Hello</div>;
+	return (
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				<Root />
+			</BrowserRouter>
+		</ThemeProvider>
+	);
 };
 
 ReactDom.render(<Index />, document.getElementById("app"));
