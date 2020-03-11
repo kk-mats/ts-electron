2c34dbf6c402e8ddd025ae5e75e6cd0db023f0a1
kk-mats <k.mats.kk@gmail.com>
2020-03-12 00:45:53 +0900

    feat: Disable nodeIntegration and enable contextIsolation

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,16 +1,30 @@
 import * as path from "path";
-import { app, BrowserWindow } from "electron";
+import * as Electron from "electron";
+
+import * as channels from "common/channels";
 
 const createWindow = (): void => {
-	const win = new BrowserWindow({
+	const win = new Electron.BrowserWindow({
 		width: 800,
 		height: 600,
 		webPreferences: {
-			nodeIntegration: true
+			nodeIntegration: false,
+			contextIsolation: true,
+			preload: path.resolve("dist", "preload.js")
 		}
 	});
 
 	win.loadFile(path.resolve("dist", "index.html"));
+
+	Electron.ipcMain.handle(
+		channels.showOpenDialog,
+		async (
+			event,
+			arg: Electron.OpenDialogOptions
+		): Promise<Electron.OpenDialogReturnValue> => {
+			return Electron.dialog.showOpenDialog(win, arg);
+		}
+	);
 };
 
-app.on("ready", createWindow);
+Electron.app.on("ready", createWindow);
